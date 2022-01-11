b2b0b9ef919e1c73b1acabe9f92359d0a2e776a3
Meenu <m1798kumari@gmail.com>
2022-01-11 19:09:19 +0530

    github

---
+++ src/firebase/FireBaseConfig.js
@@ -0,0 +1,9 @@
+export default  {
+    apiKey: "AIzaSyAr_VfPa5QaTfEwL2eZWgQFJq4aKD1Q0Go",
+    authDomain: "mygithub-85bef.firebaseapp.com",
+    projectId: "mygithub-85bef",
+    storageBucket: "mygithub-85bef.appspot.com",
+    messagingSenderId: "675678853436",
+    appId: "1:675678853436:web:7b80ca15cde9af364f2639",
+    measurementId: "G-BF40XDPZYW"
+  };
